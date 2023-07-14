337174224dcecb9383a86b930e364de456624926
Manikandan N <manikandann95@users.noreply.github.com>
2023-07-14 19:14:23 +0530

    if elif else case
    IT Support Task Remainder Program

---
+++ if_elif_else.py
@@ -0,0 +1,31 @@
+# A function is created with the def() keyword. The parameter
+# variable "time_as_string" is passed to the function through a 
+# call to the function.
+def task_reminder(time_as_string):
+
+    # The following if-elif-else block assigns various strings to
+    # the variable "task" depending on specific conditions. The
+    # test conditions are set using the == equality comparison 
+    # operator. In this case, the time passed through the 
+    # "time_as_string" parameter variable is tested as the 
+    # specific condition. So, if the time  is "11:30 a.m.", then 
+    # "task" is assigned the value: "Run TPS report".
+    if time_as_string == "8:00 a.m.":
+        task = "Check overnight backup images"
+    elif time_as_string == "11:30 a.m.":
+        task = "Run TPS report"
+    elif time_as_string == "5:30 p.m.":
+        task = "Reboot servers"
+    # The else statement is a catchall for all other values of 
+    # the "time_as_string" parameter variable not listed in the
+    # if-elif block of code.
+    else:
+        task = "Provide IT Support to employees"
+
+    # This line returns the value of "task" to the function call.
+    return task
+
+# This line calls the function and passes a parameter  
+# ("10:00 a.m.") to the function.
+print(task_reminder("10:00 a.m."))
+# Should print "Provide IT Support to employees"
