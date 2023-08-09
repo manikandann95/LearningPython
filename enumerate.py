9a268b79086b3436ecde41cda7e9d3d2b9a28482
Manikandan N <manikandann95@users.noreply.github.com>
2023-08-09 13:14:51 +0530

    Update enumerate.py
    Engineering Years

--- enumerate.py
+++ enumerate.py
@@ -1,2 +1,5 @@
 college_years = ['Freshman', 'Sophomore', 'Junior', 'Senior']
 print(list(enumerate(college_years, 2019)))
+
+clg_years = ['First Year', 'Second Year', 'Pre-Final Year', 'Final Year']
+print(list(enumerate(clg_years, 2012)))
