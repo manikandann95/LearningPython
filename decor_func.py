7fd95fce981a3cb16e81f8ffc0a4f8c57e23438d
Manikandan N <manikandann95@users.noreply.github.com>
2024-01-02 16:55:14 +0530

    Create decor_func.py

---
+++ decor_func.py
@@ -0,0 +1,14 @@
+def decor(func):
+    def wrap():
+        print("============")
+        func()
+        print("============")
+    return wrap
+#We use the @ operator above the function which we want to use a decorator function
+@decor
+def print_text():
+    print("Hello world!")
+
+print_text();
+
+#our function replacing the variable containing the function with a wrapped version.
